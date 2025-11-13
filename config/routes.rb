bcf54e553f89921ceee2c0f84a6f32f221fa78af
naoyanickf <naoyanickf@gmail.com>
2025-11-14 06:40:17 +0900

    挙動の改善

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    confirmations: "users/confirmations"
+  }
   get "dev/react"
   resource :profile, only: %i[new create edit update destroy] do
     patch :email
