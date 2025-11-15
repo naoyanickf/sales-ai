fa5e7b075e010f5fd537d708f32dfa4b893ba358
naoyanickf <naoyanickf@gmail.com>
2025-11-15 13:26:41 +0900

    fix

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -5,6 +5,8 @@ class MypageController < ApplicationController
     @new_chat = current_workspace.chats.new(user: current_user)
     @initial_message = Message.new
     @products = load_chat_products
+    default_product_id = @products.first&.id
+    @new_chat.product_id ||= default_product_id if default_product_id
     @available_sales_experts = load_workspace_sales_experts
   end
 end
